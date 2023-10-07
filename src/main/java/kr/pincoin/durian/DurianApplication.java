f40cfef42d8586531649ff2be57f22d4b2c26fe9
mairoo <jonghwa@pincoin.co.kr>
2023-10-07 16:35:08 +0900

    doc files added

--- src/main/java/kr/pincoin/durian/DurianApplication.java
+++ src/main/java/kr/pincoin/durian/DurianApplication.java
@@ -2,8 +2,12 @@ package kr.pincoin.durian;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableJpaAuditing // created, modified fields = Django compatible
+@EnableScheduling // crontab style scheduler enabled
 public class DurianApplication {
 
 	public static void main(String[] args) {
