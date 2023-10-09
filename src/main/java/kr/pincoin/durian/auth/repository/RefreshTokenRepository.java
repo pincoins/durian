a5cbd6451985bc4ec8ed3008191cbcf1a0439ce5
mairoo <jonghwa@pincoin.co.kr>
2023-10-10 03:30:21 +0900

    refresh token

--- src/main/java/kr/pincoin/durian/auth/repository/RefreshTokenRepository.java
+++ src/main/java/kr/pincoin/durian/auth/repository/RefreshTokenRepository.java
@@ -4,8 +4,6 @@ import kr.pincoin.durian.auth.domain.RefreshToken;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
 }
