9d1375d87f67f9476d231745b45f42ff0efaa56a
mairoo <jonghwa@pincoin.co.kr>
2023-10-13 22:59:50 +0900

    disable user when revoking users

--- src/main/java/kr/pincoin/durian/auth/repository/jpa/RoleRepository.java
+++ src/main/java/kr/pincoin/durian/auth/repository/jpa/RoleRepository.java
@@ -10,6 +10,6 @@ import org.springframework.transaction.annotation.Transactional;
 public interface RoleRepository extends JpaRepository<Role, Long>, RoleRepositoryQuery {
     @Transactional
     @Modifying(clearAutomatically = true, flushAutomatically = true)
-    @Query(value = "UPDATE User SET role = null WHERE role.id = :roleId")
+    @Query(value = "UPDATE User SET role = null, active = false WHERE role.id = :roleId")
     void revokeUsers(@Param("roleId") Long roleId);
 }
