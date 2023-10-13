706b4fc55f01b81f1ebef6859cf058b4418ccd98
mairoo <jonghwa@pincoin.co.kr>
2023-10-14 02:05:38 +0900

    profile domain model added

--- src/main/java/kr/pincoin/durian/common/config/WebConfig.java
+++ src/main/java/kr/pincoin/durian/common/config/WebConfig.java
@@ -1,5 +1,8 @@
 package kr.pincoin.durian.common.config;
 
+import kr.pincoin.durian.auth.domain.converter.PhoneVerifiedStatusRequestConverter;
+import kr.pincoin.durian.auth.domain.converter.ProfileDomesticRequestConverter;
+import kr.pincoin.durian.auth.domain.converter.ProfileGenderRequestConverter;
 import kr.pincoin.durian.shop.domain.conveter.*;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
@@ -10,6 +13,13 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addFormatters(FormatterRegistry registry) {
         // Using converters with @RequestParam
+
+        // auth converters
+        registry.addConverter(new PhoneVerifiedStatusRequestConverter());
+        registry.addConverter(new ProfileDomesticRequestConverter());
+        registry.addConverter(new ProfileGenderRequestConverter());
+
+        // shop converters
         registry.addConverter(new ProductStatusRequestConverter());
         registry.addConverter(new ProductStockStatusRequestConverter());
         registry.addConverter(new VoucherStatusRequestConverter());
