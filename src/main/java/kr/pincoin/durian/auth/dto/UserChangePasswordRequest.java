b3eb272938aafae472e2e8b9fe19b16b20329f70
mairoo <jonghwa@pincoin.co.kr>
2023-10-13 10:25:02 +0900

    user change password

--- src/main/java/kr/pincoin/durian/auth/dto/UserChangePasswordRequest.java
+++ src/main/java/kr/pincoin/durian/auth/dto/UserChangePasswordRequest.java
@@ -1,26 +1,26 @@
 package kr.pincoin.durian.auth.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import static kr.pincoin.durian.auth.dto.UserCreateRequest.PASSWORD_PATTERN;
+
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UserChangePasswordRequest {
     @JsonProperty("oldPassword")
-    @NotNull
-    @NotBlank
     @Size(min = 8, max = 32)
+    @Pattern(regexp = PASSWORD_PATTERN)
     private String oldPassword;
 
     @JsonProperty("newPassword")
-    @NotNull
-    @NotBlank
     @Size(min = 8, max = 32)
+    @Pattern(regexp = PASSWORD_PATTERN)
     private String newPassword;
 
     public UserChangePasswordRequest(@NotNull String oldPassword,
