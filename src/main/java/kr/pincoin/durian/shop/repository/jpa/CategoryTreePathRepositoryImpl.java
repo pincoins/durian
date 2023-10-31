82a634b14e8ffaa1ececceeb491db428078536c9
mairoo <jonghwa@pincoin.co.kr>
2023-10-31 20:22:25 +0900

    find parent ancestors method is unnecessary because of native query

--- src/main/java/kr/pincoin/durian/shop/repository/jpa/CategoryTreePathRepositoryImpl.java
+++ src/main/java/kr/pincoin/durian/shop/repository/jpa/CategoryTreePathRepositoryImpl.java
@@ -1,23 +1,9 @@
 package kr.pincoin.durian.shop.repository.jpa;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import kr.pincoin.durian.shop.domain.Category;
-import kr.pincoin.durian.shop.domain.CategoryTreePath;
 import lombok.RequiredArgsConstructor;
-
-import java.util.List;
-
-import static kr.pincoin.durian.shop.domain.QCategoryTreePath.categoryTreePath;
 
 @RequiredArgsConstructor
 public class CategoryTreePathRepositoryImpl implements CategoryTreePathRepositoryQuery {
     private final JPAQueryFactory queryFactory;
-
-    @Override
-    public List<CategoryTreePath> findParentAncestors(Category parent) {
-        return queryFactory.select(categoryTreePath)
-                .from(categoryTreePath)
-                .where(categoryTreePath.descendant.eq(parent))
-                .fetch();
-    }
 }
--- src/main/java/kr/pincoin/durian/shop/repository/jpa/CategoryTreePathRepositoryQuery.java
+++ src/main/java/kr/pincoin/durian/shop/repository/jpa/CategoryTreePathRepositoryQuery.java
@@ -1,15 +1,6 @@
 package kr.pincoin.durian.shop.repository.jpa;
 
-import kr.pincoin.durian.shop.domain.Category;
-import kr.pincoin.durian.shop.domain.CategoryTreePath;
-
-import java.util.List;
-
 public interface CategoryTreePathRepositoryQuery {
-    List<CategoryTreePath> findParentAncestors(Category parent);
-
-
-
     // findRoots
     // findParent
     // findChildren
