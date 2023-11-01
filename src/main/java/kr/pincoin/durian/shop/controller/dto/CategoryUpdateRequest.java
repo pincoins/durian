fd7cb3dbaafe0a67956a7a4bcfd6375fc49b448b
mairoo <jonghwa@pincoin.co.kr>
2023-11-01 10:51:21 +0900

    create/update category validation

--- src/main/java/kr/pincoin/durian/shop/controller/dto/CategoryUpdateRequest.java
+++ src/main/java/kr/pincoin/durian/shop/controller/dto/CategoryUpdateRequest.java
@@ -1,6 +1,9 @@
 package kr.pincoin.durian.shop.controller.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,17 +14,26 @@ import java.math.BigDecimal;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CategoryUpdateRequest {
     @JsonProperty("title")
+    @NotEmpty
+    @NotBlank
     private String title;
 
     @JsonProperty("slug")
+    @NotEmpty
+    @NotBlank
     private String slug;
 
     @JsonProperty("description")
+    @NotEmpty
+    @NotBlank
     private String description;
 
     @JsonProperty("subDescription")
+    @NotEmpty
+    @NotBlank
     private String subDescription;
 
     @JsonProperty("discountRate")
+    @DecimalMin(value = "0.0", inclusive = false)
     private BigDecimal discountRate;
 }
