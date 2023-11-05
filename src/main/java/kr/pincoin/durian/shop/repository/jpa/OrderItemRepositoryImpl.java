a5351d3ed2db8be84521fdc26b99b48e39038f85
mairoo <jonghwa@pincoin.co.kr>
2023-11-05 14:27:24 +0900

    find order items

--- src/main/java/kr/pincoin/durian/shop/repository/jpa/OrderItemRepositoryImpl.java
+++ src/main/java/kr/pincoin/durian/shop/repository/jpa/OrderItemRepositoryImpl.java
@@ -1,7 +1,47 @@
 package kr.pincoin.durian.shop.repository.jpa;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import kr.pincoin.durian.shop.domain.OrderItem;
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+import static kr.pincoin.durian.shop.domain.QOrder.order1;
+import static kr.pincoin.durian.shop.domain.QOrderItem.orderItem;
 
 @RequiredArgsConstructor
 public class OrderItemRepositoryImpl implements OrderItemRepositoryQuery {
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public List<OrderItem> findOrderItems(Long orderId,
+                                          Long userId,
+                                          Boolean orderRemoved,
+                                          Boolean removed) {
+        JPAQuery<OrderItem> contentQuery = queryFactory
+                .select(orderItem)
+                .from(orderItem)
+                .innerJoin(orderItem.order)
+                .fetchJoin()
+                .where(order1.id.eq(orderId),
+                       userIdEq(userId),
+                       orderRemovedEq(orderRemoved),
+                       removedEq(removed));
+
+        return contentQuery.fetch();
+    }
+
+    BooleanExpression removedEq(Boolean removed) {
+        return removed != null ? orderItem.removed.eq(removed) : orderItem.removed.eq(false);
+    }
+
+    BooleanExpression orderRemovedEq(Boolean orderRemoved) {
+        return orderRemoved != null ? order1.removed.eq(orderRemoved) : order1.removed.eq(false);
+    }
+
+    BooleanExpression userIdEq(Long orderId) {
+        return orderId != null ? order1.user.id.eq(orderId) : null;
+    }
 }
--- src/main/java/kr/pincoin/durian/shop/repository/jpa/OrderItemRepositoryQuery.java
+++ src/main/java/kr/pincoin/durian/shop/repository/jpa/OrderItemRepositoryQuery.java
@@ -1,4 +1,12 @@
 package kr.pincoin.durian.shop.repository.jpa;
 
+import kr.pincoin.durian.shop.domain.OrderItem;
+
+import java.util.List;
+
 public interface OrderItemRepositoryQuery {
+    List<OrderItem> findOrderItems(Long orderId,
+                                   Long userId,
+                                   Boolean orderRemoved,
+                                   Boolean removed);
 }
