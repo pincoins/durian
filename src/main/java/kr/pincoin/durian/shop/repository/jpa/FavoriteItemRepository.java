d0519b5f25d3a91af9cdb928e4cb39db68919d7a
mairoo <jonghwa@pincoin.co.kr>
2023-12-10 21:13:43 +0900

    remove cart / favorite items

--- src/main/java/kr/pincoin/durian/shop/repository/jpa/FavoriteItemRepository.java
+++
@@ -1,7 +0,0 @@
-package kr.pincoin.durian.shop.repository.jpa;
-
-import kr.pincoin.durian.shop.domain.FavoriteItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FavoriteItemRepository extends JpaRepository<FavoriteItem, Long>, FavoriteItemRepositoryQuery {
-}
